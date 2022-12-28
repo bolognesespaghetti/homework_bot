2bd87b26806187dce7f7498ccb225353321ff349
Андрей Каменко <bolognese.dev@gmail.com>
2022-12-29 03:50:25 +0400

    pep8

--- homework.py
+++ homework.py
@@ -65,7 +65,7 @@ def send_message(bot, message):
 
 def get_api_answer(timestamp):
     """Функция отправки запроса к API.
-       Возвращает запрос в формате json."""
+    Возвращает запрос в формате json."""
     try:
         payload = {'from_date': timestamp}
         request = requests.get(
@@ -84,7 +84,7 @@ def get_api_answer(timestamp):
 
 def check_response(response):
     """Функция проверяющая содержимое запроса.
-       Возвращает домашнюю работу по ключу."""
+    Возвращает домашнюю работу по ключу."""
     if not isinstance(response, dict):
         logger.error('Полученный ответ не является словарём')
         raise TypeError('Полученный ответ не является словарём')
@@ -102,7 +102,7 @@ def check_response(response):
 
 def parse_status(homework):
     """Функция прасинга статуса домашней работы.
-       Возвращает готовый ответ для сообщения бота."""
+    Возвращает готовый ответ для сообщения бота."""
     current_status = homework.get('status')
     if current_status not in HOMEWORK_VERDICTS:
         logger.error('Неожиданный статус работы')
@@ -117,7 +117,8 @@ def parse_status(homework):
 
 
 def main():
-    """Основная логика работы бота. Выполняется в порядке:
+    """Основная логика работы бота.
+    Выполняется в порядке:
     1. Проверка токинов.
     2. Запрос к API
     3. Проверка корректности ответа
